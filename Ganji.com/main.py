16c35bcaf7f1775d9f9c9105ce86be61644e2820
tookerski <278604191@qq.com>
2016-07-08 11:42:04 +0800

    Update and fix some feature and bug on 7/8: *fix channel_extract code issue. *update phone number goods links and info crawler and combined in parsing module.

--- Ganji.com/main.py
+++ Ganji.com/main.py
@@ -4,12 +4,17 @@
 
 from multiprocessing import Pool
 from channel_extract import channel_list
-from page_parsing import get_item_link
+from page_parsing import get_item_info,get_item_link,get_phone_info,get_phone_links
 
+#定义主抓取商品链接的函数
 def get_all_links(channel):
     for num in range(1,151):
-        get_item_link(channel,num)
+        if channel!="http://bj.ganji.com/shoujihaoma/":
+            get_item_link(channel,num)
+        else:
+            get_phone_links(num)
 
+#定义主启动函数
 if __name__=='__main__':
     pool = Pool()
     pool.map(get_all_links,channel_list.split())
--- Ganji.com/page_parsing.py
+++ Ganji.com/page_parsing.py
@@ -7,10 +7,13 @@ import requests
 import time
 import pymongo
 
+#建立Mongodb数据库
 client = pymongo.MongoClient("localhost",27017)
 ganji = client["ganji"]
-url_list = ganji["url_list"]
-item_info = ganji["item_info"]
+url_list = ganji["url_list"]    #非手机号商品url的表
+item_info = ganji["item_info"]  #非手机号商品信息的表
+phNum_list = ganji["phNum_list"]    #手机号商品url的表
+phNum_info = ganji["phNum_info"]    #手机号商品信息的表
 
 #spider1爬取非手机号的商品链接
 def get_item_link(channel,pages,who_seller=1):
@@ -19,7 +22,7 @@ def get_item_link(channel,pages,who_seller=1):
     web_data = requests.get(url)
     time.sleep(1)
     soup = BeautifulSoup(web_data.text,"lxml")
-    next = soup.select("div.pageBox > ul > li > a > span")
+    next = soup.select("div.pageBox > ul > li > a > span")#此处判断页面是否还有下一页，有的话才抓取
     if next!=[]:
         links = soup.select("dd.feature > div > ul > li > a")
         for link in links:
@@ -27,9 +30,7 @@ def get_item_link(channel,pages,who_seller=1):
             url_list.insert_one({'item_link':item_link})
     else:pass
 
-#get_item_link('http://bj.ganji.com/jiaju/',1)
-
-#spider2爬取手机号商品链接
+#spider2爬取手机号商品详情
 def get_phone_info(url):
     w = requests.get(url)
     soup = BeautifulSoup(w.text, "lxml")
@@ -44,7 +45,7 @@ def get_phone_info(url):
     item_info.insert_one(data)
     print(data)
 
-#spider3爬取详情页数据
+#spider3爬取非手机号商品详情页数据
 def get_item_info(url):
     w = requests.get(url)
     soup = BeautifulSoup(w.text,"lxml")
@@ -63,4 +64,17 @@ def get_item_info(url):
     #item_info.insert_one(data)
     print(data)
 
-get_item_info("http://bj.ganji.com/shoujihao/1799327353_1667867301x.htm")
+#spider4爬取手机号商品链接
+def get_phone_links(pages,channel="http://bj.ganji.com/shoujihaoma/",who_seller=1):
+    #http://bj.ganji.com/shoujihaoma/a1o2/
+    url = "{}a{}o{}".format(channel,str(who_seller), str(pages))
+    web_data = requests.get(url)
+    time.sleep(1)
+    soup = BeautifulSoup(web_data.text, "lxml")
+    next = soup.select("div.pageBox > ul > li > a > span")
+    if next != []:
+        links = soup.select("a.pn-lbox")
+        for link in links:
+            phone_link = link.get("href")
+            phNum_list.insert_one({"phone":phone_link})
+            print(phone_link)
